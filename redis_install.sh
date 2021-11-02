cabb1b353da36337e52a6b72f578bf7cab6f5119
littlemogou <aoielqj@126.com>
2021-11-02 19:24:43 +0800

    update redis_install.sh

--- redis_install.sh
+++ redis_install.sh
@@ -10,5 +10,5 @@ mkdir -p /usr/local/redis/conf/
 cp /home/redis-4.0.9/redis.conf /usr/local/redis/conf/
 sed -i '69s/127.0.0.1/0.0.0.0/' /usr/local/redis/conf/redis.conf
 sed -i '88s/protected-mode yes/protected-mode no/' /usr/local/redis/conf/redis.conf
-sed -i '136s/daemonize no/daemonize yes/' /usr/local/redis/conf/redis.conf
+# sed -i '136s/daemonize no/daemonize yes/' /usr/local/redis/conf/redis.conf
 sed -i '500s/# requirepass foobared/requirepass travelsong/' /usr/local/redis/conf/redis.conf
